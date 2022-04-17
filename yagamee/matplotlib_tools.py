d336c33a2f11e55f9e2a0c875a2516c924761b73
Mya-Mya <interleukin413@gmail.com>
2022-04-18 08:56:20 +0900

    Create `restore_rcparams`

--- yagamee/matplotlib_tools.py
+++ yagamee/matplotlib_tools.py
@@ -1,8 +1,15 @@
+from typing import Dict,Any
+default_rcparams:Dict[str,Any] = None
 def init_rcparams():
+    global default_rcparams
     import matplotlib
     from yagamee.yagamee_rcparams import yagamee_rcparams
+    default_rcparams = dict(matplotlib.rcParams)
     matplotlib.rcParams.update(yagamee_rcparams)
 
+def restore_rcparams():
+    import matplotlib
+    matplotlib.rcParams.update(default_rcparams)
 
 def init_matplotlib():
     init_rcparams()
