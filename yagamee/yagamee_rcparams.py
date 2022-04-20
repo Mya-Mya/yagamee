728579aa18ef4b2652eb18f2b747b8dc348e1db5
Mya-Mya <interleukin413@gmail.com>
2022-04-20 15:31:27 +0900

    Simplify `yagamee_rcparams`

--- yagamee/yagamee_rcparams.py
+++ yagamee/yagamee_rcparams.py
@@ -1,26 +1,35 @@
-yagamee_rcparams = {
+from typing import Literal
+from matplotlib import cycler
+
+font_defaults = {
     "font.size": 16,
+    "axes.labelsize": 20,
+}
+figure_defaults = {
     "figure.figsize": [
-        14.0,
-        7.0
+        9.0,
+        4.8
     ],
-    "xtick.labelsize": "large",
-    "ytick.labelsize": "large",
-    "axes.labelsize": "large",
+}
+tick_defaults = {
     "xtick.direction": "in",
     "ytick.direction": "in",
-    "xtick.major.size": 7,
-    "ytick.major.size": 7,
-    "axes.spines.top": True,
-    "axes.spines.right": True,
-    "axes.spines.bottom": True,
-    "axes.spines.left": True,
-    "axes.linewidth": 1.0,
-    "legend.borderpad": 0.5,
-    "legend.borderaxespad": 1.0,
+    "xtick.major.size": 10,
+    "ytick.major.size": 10,
+    "xtick.major.width": 1.5,
+    "ytick.major.width": 1.5,
+    "xtick.minor.size": 5,
+    "ytick.minor.size": 5,
+    "xtick.minor.width": 1.2,
+    "ytick.minor.width": 1.2,
+}
+legend_defaults = {
+    "legend.borderpad": .5,
+    "legend.borderaxespad": 1.,
     "legend.edgecolor": [
         0.2,
         0.2,
         0.2
     ]
 }
+yagamee_rcparams = font_defaults | figure_defaults | tick_defaults | legend_defaults
