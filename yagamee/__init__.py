28c9716a7ea5ac2fe9d0c78115abe832481968d9
Mya-Mya <omiyayimo@outlook.jp>
2022-10-24 17:44:09 +0900

    Import

--- yagamee/__init__.py
+++ yagamee/__init__.py
@@ -2,5 +2,6 @@ __version__ = "0.2.3"
 from yagamee import matplotlib_tools
 from yagamee import dataframe_tools
 from yagamee import formats
+from yagamee import numpy_tools
 
 matplotlib_tools.init_matplotlib()
