829a0ef3e236e76e2268832536856a476ea2382e
Mya-Mya <omiyayimo@outlook.jp>
2022-10-24 17:55:41 +0900

    Create text2mat test

--- tests/numpy_tools.py
+++ tests/numpy_tools.py
@@ -5,3 +5,10 @@ import yagamee
 arr = ["A","B","C","D"]
 print(yagamee.numpy_tools.index_like(arr))
 print(yagamee.numpy_tools.index_like(arr,start=2))
+
+print(yagamee.numpy_tools.text2mat("3 1 4;1 5 9;2 6 5"))
+print(yagamee.numpy_tools.text2mat("3 1 4;1 5 9;2 6 5.3"))
+print(yagamee.numpy_tools.text2mat("3 1 4;1 5 9;2 6 5.0"))
+print(yagamee.numpy_tools.text2mat("3 1 4;1 5 9;2 6 5e3"))
+print(yagamee.numpy_tools.text2mat("3 1 4;1 5 9;a b"))
+print(yagamee.numpy_tools.text2mat("3 1 4;1 5 9;2 6"))
